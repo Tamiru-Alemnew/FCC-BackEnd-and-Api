3b81682c9638c9c93ff33d7fb0d60d375ebd3c35
Replit user <>
2023-10-19 09:13:43 +0000

    initial commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+const multer  = require('multer')
+const upload = multer({ dest: 'upload/' })
 var app = express();
 
 app.use(cors());
@@ -11,7 +12,16 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post("/api/fileanalyse" , upload.single('upfile'), function (req, res) {
+  if(!req.file.size){
+    return res.send("No file is uploaded ! ")
+  }
+  return res.json({
+    "name": req.file.originalname,
+    "type": req.file.mimetype,
+    "size": req.file.size,
+  })
+})
 
 
 const port = process.env.PORT || 3000;
